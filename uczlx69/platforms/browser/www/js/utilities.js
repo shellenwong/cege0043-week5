8818d5a8fcb28616e81a4dede7660f23a6f804bb
Kris Wong <shellenwong@hotmail.com>
2019-02-09 15:41:22 -0500

    l

--- uczlx69/platforms/browser/www/js/utilities.js
+++ uczlx69/platforms/browser/www/js/utilities.js
@@ -3,17 +3,17 @@ var httpsPortNumber;
 
 function getPort(){
 	var xhr=new XMLHttpRequest();
-	xhr.addEventListener('load',function(){
+	xhr.addEventListener("load",function(){
 		var parser=new DOMParser();
-		var doc=parser.parseFromString(xhr.responseText,'application/xml');
-		httpPortNumber=doc.getElementsByTagName('node-port-http').item(0).textContent;
-		httpsPortNumber=doc.getElementsByTagName('node-port-https').item(0).textContent;
-		alert('Port:'+httpPortNumber);
+		var doc=parser.parseFromString(xhr.responseText,"application/xml");
+		httpPortNumber=doc.getElementsByTagName("node-port-http").item(0).textContent;
+		httpsPortNumber=doc.getElementsByTagName("node-port-https").item(0).textContent;
+		alert("Port:"+httpPortNumber);
 	});
 	//depending on whether we're in a browser or on a phone
 	//the location of the config file is different
 	//if we're on a phone then http and https won't be present
-	var configLocation='res/port.xml';
-	xhr.open('get',configLocation,true);
+	var configLocation="res/port.xml";
+	xhr.open("get",configLocation,true);
 	xhr.send();
 }
